f562a17bfcbad2bfc4f282f5c69dee9e1b72678e
Leon Lu <leonchina@aliyun.com>
2016-10-04 15:29:15 +0800

    jquery.album.js

--- dist/jquery.album.js
+++ dist/jquery.album.js
@@ -10,7 +10,8 @@
         var albumBorderStyle=options.albumBorderStyle;
         var albumBorderColor=options.albumBorderColor;
         var albumBorder=options.albumBorder;
-        var boxDistance=options.boxDistance;
+        var albumBgColor=options.albumBgColor;
+        var imageDistance=options.imageDistance;
         var imageBorderWidth=options.imageBorderWidth;
         var imageBorderColor=options.imageBorderColor;
         var boxArray=new Array();
@@ -22,18 +23,19 @@
             albumBorderStyle:'solid',
             albumBorderColor:'black',
             albumBorder:'0px solid black',
-            boxDistance:14,
+            albumBgColor:'black',
+            imageDistance:14,
             imageBorderWidth:0,
             imageBorderColor:'white'
         },options);
 
         //album's and box's style
-        if(rowLength!=null){
+        if(rowLength!=undefined&&imageDistance!=undefined){
             $(this).css({
                 "width":albumWidth
             })
             $('.box',this).css({
-                "margin":boxDistance/2
+                "margin":imageDistance/2
             })
             var boxWidth=$(this).width()/rowLength;
             var boxPadding;
@@ -43,24 +45,49 @@
                 boxPadding=0;
             }
             $('.box img',this).css({
-                "width":boxWidth-boxDistance-2*boxPadding
+                "width":boxWidth-imageDistance-2*boxPadding
             })}else {
-            console.error("album:rowLength should not be empty!")
+                if(rowLength==undefined){
+            console.error("album:rowLength should not be empty!");}
+            else if(imageDistance==undefined){
+                    console.error("album:imageDistance should not be empty!");
+                }
         }
-console.log(imageBorderWidth);
 
         //albumBorder
+        if(albumBorderWidth==undefined){
+            albumBorderWidth=0;
+        }
+        if(albumBorderStyle==undefined){
+            albumBorderStyle='solid';
+        }
+        if(albumBorderColor==undefined){
+            albumBorderColor='black';
+        }
+        if(albumBorder==undefined){
+            albumBorder='0 solid black';
+        }
+        if(albumBgColor==undefined){
+            albumBgColor='white';
+        }
         if(albumHeight!="auto"){
             $(this).css({
                 "border-width":albumBorderWidth,
                 "border-style":albumBorderStyle,
                 "border-color":albumBorderColor,
-                "border":albumBorder
+                "border":albumBorder,
+                "background":albumBgColor
             })}else{
             console.error("album:If albumHeight is auto,border-width,border-style,border-color and border are invalid!")
         }
 
         //imageBorder
+        if(imageBorderWidth==undefined){
+            imageBorderWidth=0;
+        }
+        if(imageBorderColor==undefined){
+            imageBorderColor='black';
+        }
         if(imageBorderWidth!=undefined){
             $('.box',this).css({
                 "padding":imageBorderWidth,
@@ -89,7 +116,7 @@ console.log(imageBorderWidth);
             })
         }
 
-        //boxs' position
+        //boxes' position
         $('.box',this).each(function (index,element) {
             var boxHeight=$(element).outerHeight(true);
             if(index<rowLength){
